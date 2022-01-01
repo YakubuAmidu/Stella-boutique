7e87e83bb3cbe3c6ef83fad76cec94e0d3044f93
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-01-01 13:59:27 -0800

    Initial commit

--- routes/products/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Product = require('../../models/Product');
-
-router.get('/', (req, res) => {
-    res.send('Product routes');
-})
-
-module.exports = router;
