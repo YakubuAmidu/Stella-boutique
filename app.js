f68373e15428510aa8936573d735a6eb358013cd
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-01-02 11:30:43 -0800

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 require('dotenv/config');
 
 const app = express();
+const morgan = require('morgan');
+
 const api = process.env.API_URL;
+
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+app.use(morgan('tiny'));
+
+// Connect to database
+connectDB();
 
 app.get(`${api}/products`, function(req, res){
   const product = {
@@ -22,9 +32,9 @@ app.post(`${api}/products`, function(req, res){
     const newProduct = req.body;
     res.send(newProduct);
     console.log(newProduct);
-})
+});
 
-app.listen(3000, function(){
-    console.log('Sever running of port:3000');
+app.listen(PORT, function(){
+    console.log(`Sever started on port ${PORT}`);
     console.log(api);
 });
