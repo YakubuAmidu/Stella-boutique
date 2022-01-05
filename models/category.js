ea03dd2a6ae37d8cf78ce30abee4a01f234e9790
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-01-05 00:00:30 -0800

    Initial commit

--- models/category.js
+++ models/category.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
-
+   name: {
+       type: String,
+       required: true,
+   },
+   icon: {
+       type: String
+   },
+   color: {
+       type: String
+   },
 });
 
 module.exports = Category = mongoose.model('Category', CategorySchema);
