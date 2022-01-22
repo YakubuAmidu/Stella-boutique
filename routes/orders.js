26a234a322667110a18052b49555ccd5f6629e66
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-01-22 14:59:53 -0800

    Initial commit

--- routes/orders.js
+++ routes/orders.js
@@ -16,4 +16,26 @@ router.get('/', async (req, res) => {
     res.send(orderList);
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const { orderItems, shippingAddress1, shippingAddress2, phone, city, country, zip, status, totalPrice } = req.body;
+        let order = new Order({
+            orderItems,
+            shippingAddress1,
+            shippingAddress2,
+            phone,
+            city,
+            country,
+            zip,
+            status,
+            totalPrice
+        });
+
+        order = await order.save();
+
+    } catch(err) {
+      console.error(err.message);
+    }
+})
+
 module.exports = router;
