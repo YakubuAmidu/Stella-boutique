5f319003c89a5d5f400ac3ad95e7c2b906373e5e
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-01-23 07:51:35 -0800

    Initial commit

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/order');
+const OrderItem = require('../models/orderItem');
 
 router.get('/', async (req, res) => {
  try {
@@ -18,10 +19,24 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const { orderItems, shippingAddress1, shippingAddress2, phone, city, country, zip, status, totalPrice } = req.body;
+        const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
+            let newOrderItems = new OrderItem({
+              quantity: orderItem.quantity,
+              product: orderItem.product
+            });
+
+            newOrderItems = await newOrderItems.save();
+
+            return newOrderItems._id;
+        }));
         
+        const orderItemResolve = await orderItemsIds;
+        console.log(orderItemResolve);
+
+        const { orderItems, shippingAddress1, shippingAddress2, phone, city, country, zip, status, totalPrice } = req.body;
+
         let order = new Order({
-            orderItems,
+            orderItems: orderItemResolve,
             shippingAddress1,
             shippingAddress2,
             phone,
@@ -33,6 +48,13 @@ router.post('/', async (req, res) => {
         });
 
         order = await order.save();
+
+        if(!order){
+            return res.status(400).json({ success: false, message: 'The order cannot be created!🚫'});
+        }
+
+        res.send(order);
+        console.log(order);
 
     } catch(err) {
       console.error(err.message);
